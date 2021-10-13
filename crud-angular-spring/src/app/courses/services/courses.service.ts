18a5111ef779ec78c91d28dfa3a4aaa6d6e931a2
tria.luism <spcbrasil.2021>
2021-10-13 16:30:25 -0300

    Aula 8 - CRUD Angular + Spring | 08 - Criando um Service no Angular

--- crud-angular-spring/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/src/app/courses/services/courses.service.ts
@@ -1,12 +1,13 @@
 import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private httpCliente: HttpClient) { }
 
   list(): Course[] {
     return [{ _id: '1', name: 'Course 1', category: 'Category 1' }];
