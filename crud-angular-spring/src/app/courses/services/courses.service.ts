dd6f026ccba9ba9db78c1b171d2bc5f1b7b6cab5
Luís Filipe Guedes Mota <filipemot@gmail.com>
2021-12-21 15:30:30 -0300

    Configure APP

--- crud-angular-spring/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/src/app/courses/services/courses.service.ts
@@ -1,22 +1,24 @@
-import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { delay, first } from 'rxjs/operators';
+
+import { Course } from '../model/course';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
-  constructor(private httpCliente: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   list() {
-    return this.httpCliente.get<Course[]>(this.API).pipe(
-      first()
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      // delay(5000),
+      tap(courses => console.log(courses))
     );
   }
 }
-
-
