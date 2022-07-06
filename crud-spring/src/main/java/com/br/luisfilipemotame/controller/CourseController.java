7bfe48c996d3d086ff0ca246efbaabc55a79877e
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-07-06 12:20:14 -0300

    CRUD Angular + Spring | 21: Formulário: API Spring: Criar Curso (HTTP POST)

--- crud-spring/src/main/java/com/br/luisfilipemotame/controller/CourseController.java
+++ crud-spring/src/main/java/com/br/luisfilipemotame/controller/CourseController.java
@@ -1,24 +1,40 @@
 package com.br.luisfilipemotame.controller;
+import java.util.List;
 
 import com.br.luisfilipemotame.model.Course;
 import com.br.luisfilipemotame.repository.CourseRepository;
-import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
 public class CourseController {
 
-    private CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
+    //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public @ResponseBody List<Course> list(){
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        //return courseRepository.save(course);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(courseRepository.save(course));
+    }
+
 }
