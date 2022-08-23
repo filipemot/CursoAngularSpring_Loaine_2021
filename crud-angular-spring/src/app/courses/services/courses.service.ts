2d23a131b83f97f84f4c4d5c4a4f42a161585d7b
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-08-23 15:51:11 -0300

    Aula 23 - CRUD Angular + Spring | 23 - Angular Typed Forms

--- crud-angular-spring/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/src/app/courses/services/courses.service.ts
@@ -15,14 +15,18 @@ export class CoursesService {
 
   list() {
     return this.httpClient.get<Course[]>(this.API)
-    .pipe(
-      first(),
-      // delay(5000),
-      tap(courses => console.log(courses))
-    );
+      .pipe(
+        first(),
+        //delay(5000),
+        tap(courses => console.log(courses))
+      );
   }
 
-  save(record: Course) {
+  loadById(id: string) {
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
+  save(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
--- crud-spring/src/main/java/com/br/luisfilipemotame/controller/CourseController.java
+++ crud-spring/src/main/java/com/br/luisfilipemotame/controller/CourseController.java
@@ -4,6 +4,7 @@ import com.br.luisfilipemotame.model.Course;
 import com.br.luisfilipemotame.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,20 +16,27 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    //@RequestMapping(method = RequestMethod.GET)
+    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
 
-    //@RequestMapping(method = RequestMethod.POST)
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         // System.out.println(course.getName());
         return courseRepository.save(course);
         // return ResponseEntity.status(HttpStatus.CREATED)
-        //     .body(courseRepository.save(course));
+        // .body(courseRepository.save(course));
     }
 
 }
