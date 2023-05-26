2a35079a738742d5c84b10deaa00155542fc7819
Luis Filipe Guedes Mota <filipemot@gmail.com>
2023-05-26 12:36:47 -0300

    aula 45 - hasMany

--- crud-spring/src/main/java/com/br/luisfilipemotame/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/br/luisfilipemotame/CrudSpringApplication.java
@@ -2,6 +2,7 @@ package com.br.luisfilipemotame;
 
 import com.br.luisfilipemotame.enums.Category;
 import com.br.luisfilipemotame.model.Course;
+import com.br.luisfilipemotame.model.Lesson;
 import com.br.luisfilipemotame.repository.CourseRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -23,6 +24,12 @@ public class CrudSpringApplication {
 			Course c = new Course();
 			c.setName("Angular com Spring");
 			c.setCategory(Category.BACK_END);
+
+			Lesson l = new Lesson();
+			l.setName("Introdução");
+			l.setYoutubeUrl("watch?v=1");
+			l.setCourse(c);
+			c.getLessons().add(l);
 
 			courseRepository.save(c);
 		};
