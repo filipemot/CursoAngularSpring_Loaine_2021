9d9e14fa82b59934855750d1258b310f674d141c
Luis Filipe Guedes Mota <filipemot@gmail.com>
2023-08-23 18:32:47 -0300

    56: mensagem de validação

--- crud-spring/src/main/java/com/br/luisfilipemotame/model/Lesson.java
+++ crud-spring/src/main/java/com/br/luisfilipemotame/model/Lesson.java
@@ -9,9 +9,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
-@Data
 @Entity
 public class Lesson {
 
@@ -19,14 +20,62 @@ public class Lesson {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @NotBlank
+    @Length(min = 5, max = 100)
     @Column(length = 100, nullable = false)
     private String name;
 
+    @NotNull
+    @NotBlank
+    @Length(min = 10, max = 11)
     @Column(length = 11, nullable = false)
     private String youtubeUrl;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Course course;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYoutubeUrl() {
+        return youtubeUrl;
+    }
+
+    public void setYoutubeUrl(String youtubeUrl) {
+        this.youtubeUrl = youtubeUrl;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Lesson [id=").append(id).append(", name=").append(name).append(", youtubeUrl=")
+                .append(youtubeUrl).append(", course=").append(course).append("]");
+        return builder.toString();
+    }
+
 }
