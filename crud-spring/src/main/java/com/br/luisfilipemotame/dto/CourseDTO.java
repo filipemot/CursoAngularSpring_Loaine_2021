072075fd265d96c739f3b7a4cfb65d10580292d0
Luis Filipe Guedes Mota <filipemot@gmail.com>
2023-08-28 09:50:35 -0300

    Aula 53 - CRUD Angular + Spring | 53: Classe Utilitária para Validação de Formulários

--- crud-spring/src/main/java/com/br/luisfilipemotame/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/br/luisfilipemotame/dto/CourseDTO.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 import java.util.List;
